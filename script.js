f7d19a9497f5dc1f7a971e84192255caa029c8d3
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2024-05-23 18:21:40 +0100

    Add first variables and operate function

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ function divide(a, b) {
     return a / b;
 }
 
+let num1;
+let operator;
+let num2;
+
+function operate(num1, operator, num2) {
+    let answer;
+    switch (operator) {
+        case "plus":
+            answer = add(num1, num2);
+            break;
+        case "minus":
+            answer = subtract(num1, num2);
+            break;
+        case "times":
+            answer = multiply(num1, num2);
+            break;
+        case "division":
+            answer = divide(num1, num2);
+            break;
+    };
+    return answer;
+};
+
+let answer = operate(6, "plus", 3);
+console.log(answer);
